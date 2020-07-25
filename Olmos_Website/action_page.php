2c64b83e7b253e2c5fe16d871b9faaa640a0031d
Parkour3007 <joshuashterenberg@gmail.com>
2020-07-25 13:07:38 -0500

    PHP updates

--- Olmos_Website/action_page.php
+++ Olmos_Website/action_page.php
@@ -1,18 +1,53 @@
 <?php 
-if(isset($_POST['submit'])){
-    $to = "olmosensemble@gmail.com"; // this is your Email address
-    $from = $_POST['email']; // this is the sender's Email address
-    $name = $_POST['full_name'];
-    $subject = "Form submission";
-    $subject2 = "Copy of your form submission";
-    $message = $full_name . " sent you the following message from olmosensemble.com:" . "\n\n" . $_POST['message'];
-    $message2 = "Here is a copy of your message " . $full_name . "\n\n" . $_POST['message'];
+function IsInjected($str) { //Injection protection
+    $injections = array('(\n+)',
+           '(\r+)',
+           '(\t+)',
+           '(%0A+)',
+           '(%0D+)',
+           '(%08+)',
+           '(%09+)'
+           );
+               
+    $inject = join('|', $injections);
+    $inject = "/$inject/i";
+    
+    if (preg_match($inject,$str)) {
+      return true;
+    } else {
+      return false;
+    }
+}
 
+if (isset($_POST['submit'])){ //Main, when submit is pressed
+    
+    $to = "olmosensemble@gmail.com";
+    
+    $from = $_POST['email']; // this is the sender's info from the form (names on contact.html)
+    $name = $_POST['full_name'];
+    $subject = $_POST['emailsubject'];
+    $message = $name . " sent you the following message from the online form on olmosensemble.com:" . "\n\n" . $_POST['message'];
     $headers = "From:" . $from;
+    
+    if (IsInjected($from)) { //Bad value protection
+        echo "Bad email value!";
+        exit;
+    } else if (empty($name) || empty($visitor_email)) {
+        echo "Name and email are mandatory!";
+        exit;
+    }
+    
+    $subject2 = "Copy of Your Form Submission"; //info for user copy
+    $message2 = "Here is a copy of your message, " . $full_name . "\n\n" . $_POST['message'];
     $headers2 = "From:" . $to;
+    
+    
     mail($to,$subject,$message,$headers);
     mail($from,$subject2,$message2,$headers2); // sends a copy of the message to the sender
-    echo "Mail Sent. Thank you " . $full_name . ", we will contact you shortly.";
-    // You can also use header('Location: thank_you.php'); to redirect to another page.
-    }
+    
+    echo "Mail Sent. Thank you " . $full_name . ", we will contact you shortly."; //success form
+    
+} else {
+    echo "Error: you need to submit the form!";
+}
 ?>
